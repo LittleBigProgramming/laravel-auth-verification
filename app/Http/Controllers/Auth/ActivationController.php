d6ecd6c251102c1ad232da30b6acaa9a96b45bd2
Andrew Alban <andy_alban@outlook.com>
2018-11-06 12:49:37 +0000

    activation url in email works and signs user in, fixed 404 error caused by syntax in the email view

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ActivationController extends Controller
 {
-    public function activate()
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function activate(Request $request)
     {
+        $user = User::where('email', $request->email)->where('activation_token', $request->token)->firstOrFail();
 
+        $user->update([
+            'is_active' => true,
+            'activation_token' => null
+        ]);
+
+        Auth::loginUsingId($user->id);
+
+        return redirect()->route('home')->withSuccess('Activated account successfully! You\'re now signd in');
     }
 }
--- resources/views/emails/auth/activation.blade.php
+++ resources/views/emails/auth/activation.blade.php
@@ -4,9 +4,9 @@
 To get started we need you to activate your email. Please click the button below.
 
 @component('mail::button', ['url' => route('auth.activate', [
-    'token' => $user->activation_token]),
+    'token' => $user->activation_token,
     'email' => $user->email
-    ])
+    ])])
     Activate Now
 @endcomponent
 
